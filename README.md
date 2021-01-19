c91d3cd69e8b2b88c7fac5f1c8498668695b1180
Eduardo <eduar2.moreira@gmail.com>
2021-01-19 18:31:28 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# doctor-coworking# ap-coworking-frontend
---
+++ build/precache-manifest.7b05937edcf6accc5a6a119ca5511c49.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3a8e758761e78d28161340e44f33531",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e00097af9edefe818cc9",
+    "url": "/static/css/main.a48657f5.chunk.css"
+  },
+  {
+    "revision": "4e19a79f0d6d479fb37e",
+    "url": "/static/js/2.32912d13.chunk.js"
+  },
+  {
+    "revision": "5b1a7f76e875b746f87ecc74fd6ded71",
+    "url": "/static/js/2.32912d13.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e00097af9edefe818cc9",
+    "url": "/static/js/main.7306a6d0.chunk.js"
+  },
+  {
+    "revision": "b9836f246709c189bf56",
+    "url": "/static/js/runtime-main.a0a6bc6a.js"
+  },
+  {
+    "revision": "ed0c33b592f9117f4d1040317f7eac67",
+    "url": "/static/media/about-img.ed0c33b5.jpg"
+  },
+  {
+    "revision": "3f99870091fa452f01840d2f1e487020",
+    "url": "/static/media/about-img1.3f998700.jpg"
+  },
+  {
+    "revision": "94e9f2ef166ee9720a9ac6bf6ccae362",
+    "url": "/static/media/arden.94e9f2ef.png"
+  },
+  {
+    "revision": "a4e27a009540e78426c15b9b8836a765",
+    "url": "/static/media/blockqoute-bg.a4e27a00.jpg"
+  },
+  {
+    "revision": "1fa205bafbeec24ee31c549a6eadfbd4",
+    "url": "/static/media/bread-bg.1fa205ba.jpg"
+  },
+  {
+    "revision": "e8d9f9b9d5f967a8bc938aed6252e6b6",
+    "url": "/static/media/call-bg.e8d9f9b9.jpg"
+  },
+  {
+    "revision": "bb29240055987c80cf17ba1aa3f65528",
+    "url": "/static/media/client-bg.bb292400.jpg"
+  },
+  {
+    "revision": "cfff2d9218c2deeb7fc48f1b3f29ccfa",
+    "url": "/static/media/contact-img.cfff2d92.png"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "431b6362c6c2c8e1e6762e80ccc7f609",
+    "url": "/static/media/fun-bg.431b6362.jpg"
+  },
+  {
+    "revision": "50a4ab76e700a83e649be213f820fbbd",
+    "url": "/static/media/icofont.50a4ab76.woff2"
+  },
+  {
+    "revision": "f6ab04aed30a8643bf94fe00f7ff0b59",
+    "url": "/static/media/icofont.f6ab04ae.woff"
+  },
+  {
+    "revision": "011209e9779c424cb41330e384aaf1f4",
+    "url": "/static/media/mission-img.011209e9.jpg"
+  },
+  {
+    "revision": "1512d956598b53edbea61e380c83e6ea",
+    "url": "/static/media/signup-bg.1512d956.jpg"
+  },
+  {
+    "revision": "432e9174357245b8185fc058b5c33a12",
+    "url": "/static/media/slider.432e9174.jpg"
+  },
+  {
+    "revision": "0f48ecb0485ccf1b9ba629d5041ce48e",
+    "url": "/static/media/testi-bg.0f48ecb0.jpg"
+  },
+  {
+    "revision": "1fa205bafbeec24ee31c549a6eadfbd4",
+    "url": "/static/media/video-bg.1fa205ba.jpg"
+  }
+]);
